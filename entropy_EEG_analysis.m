e107ed16c4ff8debb5de6eb7b6e2f6ca27b4e82f
cchristosgalanis <galanischris2@yahoo.com>
2026-03-04 19:15:36 +0200

    Add Ami and FNN calculation methods, plots and analysis documentationo

--- entropy_EEG_analysis.m
+++ entropy_EEG_analysis.m
@@ -14,6 +14,8 @@ function entropy_EEG_analysis
         error("Error while loading BonnEGGdata.mat file. Proccess has been terminated");
     end
 
+
+    fprintf("\n--- Compute Entropy ---")
     %using Shannon's equation to determine the entropy 
     try
         %calculating entropy for category Z, category:-> healthy, open eyes
@@ -38,27 +40,88 @@ function entropy_EEG_analysis
         error("An occured while calculating entropy for categories. Process has been terminated.")
     end
 
-    %displaying calculated values for each category
-    fprintf("Entropy Values:\n")
-    fprintf("Calculated entropies for category Z are:")
-    fprintf("%4f ",entropyZ)
-    fprintf("\n")
-    fprintf("\n Calculated entropies for category O are:")
-    fprintf("%4f ",entropyO)
-    fprintf("\n")
-    fprintf("\n Calculated entropies for category N are:")
-    fprintf("%4f ",entropyN)
-    fprintf("\n")
-    fprintf("\n Calculated entropies for category F are:")
-    fprintf("%4f ", entropyF)
-    fprintf("\n")
-    fprintf("\n Calculated entropies for category S are:")
-    fprintf("%4f ", entropyS)
-    fprintf("\n")
-
     %call EnPlot for entropies
     EnPlot(entropyZ,entropyO,entropyN,entropyF,entropyS);
 
+% ------ Non Linear Analysis -------
+    
+    fprintf('\n--- Starting Non Linear Analysis  ---\n');
+    
+    % max_tau 
+    max_tau = 50;
+    max_dim = 15;
+    
+    categories = {Z_EEG_data, O_EEG_data, N_EEG_data, F_EEG_data, S_EEG_data};
+    names = {'Z (Healthy Open)', 'O (Healthy Closed)', 'N (Inter-Hemi)', 'F (Inter-Focus)', 'S (Ictal)'};
+    colors = {'b', 'w', 'g', 'm', 'r'};
+    
+    %figure for AMI curve
+    ami_fig = figure('Name', 'Average Mutual Information Analysis','Color','w');
+    hold on; grid on;
+    title('Average Mutual Information (AMI)');
+    xlabel('Time Delay (\tau)');
+    ylabel('AMI (bits)');
+
+
+    %figure for FNN curve
+    fnn_fig = figure('Name',"FNN Analysis",'Color','w');
+    title('FNN Fractiono vs Embedding Dimension');
+    xlabel("Embedding Dimension");
+    ylabel("FNN (%)");
+    grid on; hold on;
+    
+    % Loop through each category
+    for i = 1:length(categories)
+        current_data = categories{i};
+        current_name = names{i};
+        current_color = colors{i};
+        
+        fprintf('Processing Category: %s ...\n', current_name);
+        
+        %calculate ami matrix
+        ami_matrix = Compute_AMI_Matrix(current_data, max_tau);
+        
+        %finding optimal tau for each category
+        tau_vector = Find_Tau_From_Matrix(ami_matrix);
+        
+        fprintf(' Average Optimal Tau: %.2f\n', mean(tau_vector));
+        
+        %plot for curve
+        figure(ami_fig);
+        Plot_AMI_Curve(ami_matrix, current_name, current_color);
+
+        %finding optimal embedding dimension m
+        [num_signal,~] = size(current_data);
+        m_vector = zeros(num_signal,1);
+        fnn_matrix = zeros(num_signal,max_dim);
+
+        for s = 1:num_signal
+            signal = current_data(s,:);
+            tau = tau_vector(s);
+
+            [opt_m,fnn_curve] = embeddingDimensionFnn(signal,tau,max_dim);
+            m_vector(s) = opt_m;
+            fnn_matrix(s,:) = fnn_curve;
+        end
+        fprintf(" Average Embedding Dimension: %.2f\n", mean(m_vector));
+
+        figure(fnn_fig);
+        plotFNN(fnn_matrix,current_name,current_color);
+    end
+    
+    %final for AMI
+    figure(ami_fig);
+    legend('Location', 'northeast');
+    hold off;
+    %saveas(gcf, 'AMI_Analysis_Plot.png');
+
+    %final for FNN
+    figure(fnn_fig);
+    legend('Location','northeast');
+    hold off;
+    %saveas(gcf, 'FNN_Analysis_Plot.png');
+    disp('Non Linear Analysis has been completed');
+    
 end
 
 %function for calculate entropy for each category
@@ -92,9 +155,9 @@ function EnPlot(enZ,enO,enN,enF,enS)
     categories = length(category_label);
 
     category_name = {'Z','O','N','F','S'}; %using each category for plot names
-    colours = {'b', 'k', 'g', 'm', 'r'}; %colors for each category
+    colours = {'b', 'w', 'g', 'm', 'r'}; %colors for each category
     
-    %creating figure
+    %creating figure for Shannon Entropy
     figure('Name','EEG Entropy Distribution');
     hold on;
     grid on;
@@ -135,4 +198,150 @@ function EnPlot(enZ,enO,enN,enF,enS)
     end
     legend('Location','northeast')
     hold off;
+    %saveas(gcf, 'Shannon_Entropy.png');
+end
+
+% ----- Functions for AMI calculation -----
+
+% function to calculate AMI matrix
+function ami_matrix = Compute_AMI_Matrix(signal_data, max_tau)
+    % Get dimensions
+    [num_signals, ~] = size(signal_data);
+    n_bins = 32;
+    
+    ami_matrix = zeros(num_signals, max_tau);
+    
+    % Loop through each signal
+    for s = 1:num_signals
+        signal = signal_data(s, :);
+        
+        
+        for t = 1:max_tau
+            %shifted vectors
+            x = signal(1:end-t);
+            y = signal(t+1:end);
+            
+            % 2D Histogram
+            N = histcounts2(x, y, n_bins);
+            p_xy = N / sum(N(:));
+            
+            % Marginals
+            p_x = sum(p_xy, 2);
+            p_y = sum(p_xy, 1);
+            
+            %ami's calculation
+            px_py = p_x * p_y;
+            nzs = p_xy > 0;
+            ami_matrix(s, t) = sum(p_xy(nzs) .* log2(p_xy(nzs) ./ px_py(nzs)));
+        end
+    end
+end
+
+% function to calculate optimal tau for each signal category
+function optimal_tau_vec = Find_Tau_From_Matrix(ami_matrix)
+    [num_signals, max_tau] = size(ami_matrix);
+    optimal_tau_vec = zeros(num_signals, 1);
+    
+    for s = 1:num_signals
+        % get the AMI curve for this specific signal
+        curve = ami_matrix(s, :);
+        
+        % Find first local minimum
+        found_tau = 1; 
+        for t = 2:(max_tau - 1)
+            if curve(t) < curve(t-1) && curve(t) < curve(t+1)
+                found_tau = t;
+                break; 
+            end
+        end
+        optimal_tau_vec(s) = found_tau;
+    end
+end
+
+% function to plot ami curve
+function Plot_AMI_Curve(ami_matrix, name, color)
+    % mean value for each category
+    mean_curve = mean(ami_matrix);
+    
+    x_axis = 1:length(mean_curve);
+    
+    plot(x_axis, mean_curve, 'Color', color, 'LineWidth', 2, 'DisplayName', name);
+end
+
+
+%function to calculate embedding (m)
+function [optimal_m, fnn_ratio] = embeddingDimensionFnn(signal, tau, max_dim)
+    % Kennel's implementation parameters
+    r_tol = 15;  % distance tolerance
+    a_tol = 2;   % attractor tolerance
+
+    N = length(signal);
+    fnn_ratio = zeros(1, max_dim);
+    sigma = std(signal); % standard deviation for normalization
+
+    for m = 1:max_dim
+        m_curr = N - m*tau;
+        
+        Y = zeros(m_curr, m);
+        for i = 1:m
+            
+            Y(:, i) = signal(1 + (i-1)*tau : m_curr + (i-1)*tau);
+        end
+
+        % using knnsearch
+        [idx, dist_matrix] = knnsearch(Y, Y, 'K', 2);
+
+        nearest_idx = idx(:, 2);       % index of neighbor
+        dist_in_m = dist_matrix(:, 2); % distance in dimension m
+
+        false_count = 0;
+        next_tau = m * tau;
+
+        for k = 1:m_curr
+            % value on next dimension (m+1)
+            val_point_next = signal(k + next_tau);
+            
+            val_neighbor_next = signal(nearest_idx(k) + next_tau);
+
+            % distance increase in next dimension
+            dist_increase = abs(val_point_next - val_neighbor_next);
+
+            % distance drops down of machine number
+            if dist_in_m(k) < 1e-10
+                r_i = 0; % Avoid division by zero
+            else
+                r_i = dist_increase / dist_in_m(k);
+            end
+
+            % get to attractor's tolerance size
+            s_i = dist_increase / sigma;
+
+            if (r_i > r_tol) || (s_i > a_tol)
+                false_count = false_count + 1;
+            end
+        end
+        
+        % matrix of ratios
+        fnn_ratio(m) = (false_count / m_curr) * 100; 
+    end
+    
+    % find first m drops down to 1% 
+    threshold = 1; % 1% threshold
+    opt_m = find(fnn_ratio < threshold);
+    
+    if isempty(opt_m)
+        % if never drops down the threshhold
+        [~, optimal_m] = min(fnn_ratio);
+    else
+        optimal_m = opt_m(1);
+    end
+end
+
+%function to plot fnn
+function plotFNN(fnn_matrix,name,color)
+    mean_curve = mean(fnn_matrix);
+
+    x_axis = 1:length(mean_curve);
+
+    plot(x_axis,mean_curve,'Color',color,'LineWidth',2,'DisplayName',name);
 end
